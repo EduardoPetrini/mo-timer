f51a82a6419adb3796eeb378b6cc1ff21ccae1ab
EduardoPetrini <eduardo.petrini@outlook.com>
2024-07-24 23:17:05 -0300

    chore: music component

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,5 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  </template>
+  <div class="mt-10 absolute bottom-10">
+    <iframe style="border-radius: 12px" src="https://open.spotify.com/embed/playlist/6sGPclX1FuHmWjn6yJemng?utm_source=generator&theme=0" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  </div>
+</template>
