76c482097c1f0283821a92b0899488d29136c1a6
EduardoPetrini <eduardo.petrini@outlook.com>
2024-07-25 16:46:26 -0300

    chore: change the background image component

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { ref } from 'vue';
 import TimerCounter from './components/TimerCounter.vue';
 import MusicPlayer from './components/MusicPlayer.vue';
+import BackgroundImage from './components/BackgroundImage.vue';
 import FullScreen from './components/FullScreen.vue';
 
 const isPlaying = ref(false);
+const bgImage = ref("url('/wave-pattern.svg')");
 
 function playEvent(play) {
   isPlaying.value = play;
@@ -12,9 +14,10 @@ function playEvent(play) {
 </script>
 
 <template>
-  <div class="p-2 h-screen flex justify-center items-center bg-black" style="background-image: url('/wave-pattern.svg')">
+  <div class="p-2 h-screen flex justify-center items-center bg-black" :style="{ 'background-image': bgImage }">
     <TimerCounter @play="playEvent" />
     <MusicPlayer :isPlaying="isPlaying"/>
+    <BackgroundImage />
     <FullScreen  />
   </div>
 </template>
