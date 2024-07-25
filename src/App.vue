cda96a140c759f6717449497afbb418c64655ddc
EduardoPetrini <eduardo.petrini@outlook.com>
2024-07-24 22:43:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TimerCounter from './components/TimerCounter.vue'
+</script>
+
+<template>
+  <div class="p-2 h-screen flex justify-center items-center bg-gray-600" style="background-image: url('/wave-pattern.svg')" >
+    <TimerCounter />
+  </div>
+</template>
---
+++ src/components/TimerCounter.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { ref, watchEffect } from 'vue';
+const isPlaying = ref(true);
+const second = ref(0);
+const minute = ref(60);
+const hour = ref(0);
+let intervalId;
+
+const startResume = () => {
+  const interval = setInterval(() => {
+    if (second.value === 0) {
+      second.value = 59;
+      return;
+    }
+    second.value--;
+  }, 1000);
+
+  return interval;
+};
+
+watchEffect(() => {
+  if (!isPlaying.value) {
+    return;
+  }
+
+  if (second.value <= 0 && minute.value <= 0 && hour.value <= 0) {
+    return clearInterval(intervalId);
+  }
+
+  if (second.value <= 0) {
+    setTimeout(() => {
+      if (minute.value === 0) {
+        hour.value--;
+        minute.value = 59;
+        return;
+      }
+      minute.value--;
+      second.value = 59;
+    }, 1000);
+  }
+});
+
+function togglePlay() {
+  const isTimeRunning = isPlaying.value;
+  isPlaying.value = !isPlaying.value;
+
+  if (isTimeRunning) {
+    return clearInterval(intervalId);
+  }
+
+  intervalId = startResume();
+}
+
+function reset() {
+  clearInterval(intervalId);
+  isPlaying.value = false;
+
+  second.value = 0;
+  minute.value = 60;
+  hour.value = 0;
+}
+
+intervalId = startResume();
+</script>
+
+<template>
+  <div>
+    <div class="flex text-6xl font-bold font-mono p-4 bg-gray-200/80 rounded-2xl">
+      <span>{{ hour.toString().padStart(2, '0') }}:</span>
+      <span>{{ minute.toString().padStart(2, '0') }}:</span>
+      <span>{{ second.toString().padStart(2, '0') }}</span>
+    </div>
+    <div class="flex gap-4 text-gray-200 justify-around mt-4">
+      <button v-show="!isPlaying" @click="togglePlay">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+        </svg>
+      </button>
+      <button v-show="isPlaying" @click="togglePlay">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" class="size-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+        </svg>
+      </button>
+      <button @click="reset">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-10">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+      </button>
+    </div>
+    <div class="mt-10 absolute bottom-10">
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1buR1viIOgrYIWWX4j14gL?utm_source=generator&theme=0" width="100%" height="100" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  </div>
+</template>
