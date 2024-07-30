9a05d6da2ab787a132611cac0630a51150cc4a03
EduardoPetrini <eduardo.petrini@outlook.com>
2024-07-30 20:47:30 -0300

    fix: tab buttons overlap

--- src/components/TimerTab.vue
+++ src/components/TimerTab.vue
@@ -15,8 +15,8 @@ function tabSelect(value) {
 
 <template>
   <div class="flex text-gray-200 justify-between p-2">
-    <button class="btn w-full" @click="tabSelect(false)" :class="{ 'btn-active': !isAsc }">Timer</button>
-    <button class="btn w-full" @click="tabSelect(true)" :class="{ 'btn-active': isAsc }">Infinite</button>
+    <button class="btn w-full" @click="tabSelect(false)" :class="{ 'btn-active': !isAsc, 'scale-90': isAsc }">Timer</button>
+    <button class="btn w-full" @click="tabSelect(true)" :class="{ 'btn-active': isAsc, 'scale-90': !isAsc }">Infinite</button>
   </div>
   <TimerCounter v-show="!isAsc" @play="(...args) => emit('play', ...args)" :isRequestTogglePlayChanged="isRequestTogglePlayChanged" />
   <AscCounter v-show="isAsc" @play="(...args) => emit('play', ...args)" :isRequestTogglePlayChanged="isRequestTogglePlayChanged" />
