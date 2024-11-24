faf56cf7166772985aba9af5afedfd8d4ccdb083
EduardoPetrini <eduardo.petrini@outlook.com>
2024-11-24 10:15:19 -0300

    chore: header buttons hover effect

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,8 +22,8 @@ function goHome() {
       <h1 class="font-sans font-thin text-lg cursor-pointer" @click="goHome">Mo Timer</h1>
     </div>
     <div class="flex gap-4">
-      <button class="border-2 border-gray-400 rounded-md px-2" @click="goPlaylist">Playlists</button>
-      <button class="border-2 border-gray-400 rounded-md px-2" @click="goWallpapers">Wallpapers</button>
+      <button class="border-2 border-gray-400 rounded-md px-2 hover:border-gray-200 hover:scale-105 transition ease-out duration-75" @click="goPlaylist">Playlists</button>
+      <button class="border-2 border-gray-400 rounded-md px-2 hover:border-gray-200 hover:scale-105 transition ease-out duration-75" @click="goWallpapers">Wallpapers</button>
     </div>
   </div>
 </template>
