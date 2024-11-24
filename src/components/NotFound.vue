b17e4c3e781c1324690d1fc1d9d73cc1864a5546
EduardoPetrini <eduardo.petrini@outlook.com>
2024-11-24 10:34:31 -0300

    chore: add not found component layout

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goHome() {
+  router.push('/');
+}
+</script>
 
 <template>
-  NotFound
+  <div class="bg-gray-800/80 w-80 flex justify-center align-middle text-white p-10 cursor-pointer" @click="goHome">NotFound</div>
 </template>
