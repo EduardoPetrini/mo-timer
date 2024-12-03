ea34e8af2813d4f7b2f99813dfafec29220b5191
EduardoPetrini <eduardo.petrini@outlook.com>
2024-12-03 17:05:00 -0300

    fix: bg change event emit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,17 @@ import FullScreen from './FullScreen.vue';
 import BackgroundImage from './BackgroundImage.vue';
 
 const emit = defineEmits(['changeBackground']);
+
+function changeBackground() {
+  emit('changeBackground')
+}
 </script>
 <template>
   <div class="mb-0 p-4 w-full">
     
     <div class="flex flex-col gap-4 align-middle items-end">
       <FullScreen />
-      <BackgroundImage @changeBackground="(...args) => emit(...args)" />
+      <BackgroundImage @changeBackground="changeBackground" />
     </div>
   </div>
 </template>
